35fdeee7a1b96d6ddfcaecb5e588a864290606a7
Lorenzo Bonatto <lorenzobonattop@gmail.com>
2025-06-13 10:41:55 -0300

    fix: adicionando o SaleResponseDTO e SaleUpdateDTO corretamente

--- src/main/java/com/lorenzodev/concessionaria/dto/user/UserUpdateDTO.java
+++ src/main/java/com/lorenzodev/concessionaria/dto/user/UserUpdateDTO.java
@@ -1,9 +1,9 @@
 package com.lorenzodev.concessionaria.dto.user;
 
+import java.time.LocalDateTime;
+
 import com.lorenzodev.concessionaria.models.enums.UserRole;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,20 +15,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserUpdateDTO {
     private String name;
-
-    @Email(message = "Email inválido")
     private String email;
-
-    @Pattern(
-        regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
-        message = "A senha deve conter pelo menos 8 caracteres, uma letra maiúscula, uma minúscula, um número e um caractere especial"
-    )
     private String password;
-
-    private UserRole role;
-
-    @Pattern(regexp = "^\\+?[1-9][0-9]{10,14}$", message = "Formato de telefone inválido")
     private String phone;
-
     private Boolean isActive;
-} 
+    private UserRole role;
+    private LocalDateTime updatedAt;
+}
